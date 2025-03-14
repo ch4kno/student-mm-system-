635d1f71a90bfdee8070f425e8b0c00da01f227b
Ch4kno <you@example.comkanvanno62@gmail.com>
2025-03-14 14:24:41 +0700

    Add add_student.py and update_student.py

--- display_student.py
+++
@@ -1,9 +0,0 @@
-from storage import load_students
-
-def display_students():
-    students = load_students()
-    if students : 
-        for student  in students:
-            print(students)
-if __name__ == "__main__":
-    display_students()
